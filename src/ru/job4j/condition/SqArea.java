b4dc6fb36c7d60b62b7770cc762e485839df47d8
TkachevArtem <art599@yandex.ru>
2020-09-07 10:41:53 +0300

    0. Операции сравнения [#355499]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,15 +1,15 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        int h = p / ((k + 1) * 2);
-        int l = h * k;
+    public static double square(double p, int k) {
+        double h = p / ((k + 1) * 2);
+        double l = h * k;
         double rsl = l * h;
         return rsl;
     }
 
     public static void main(String[] args) {
-        double result1 = SqArea.square(4, 1);
+        double result1 = SqArea.square(5, 1);
         double result2 = SqArea.square(6, 2);
 
         System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
