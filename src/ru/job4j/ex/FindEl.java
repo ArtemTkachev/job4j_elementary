7a7a616589fac8ee333737cf8922dc89bb4bc0c6
TkachevArtem <art599@yandex.ru>
2020-09-24 18:43:16 +0300

    0.4. Пользовательские исключения. [#355679]

---
+++ src/ru/job4j/ex/FindEl.java
@@ -0,0 +1,20 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                return i;
+            }
+        }
+        throw new ElementNotFoundException("Element not found in array of String");
+    }
+
+    public static void main(String[] args) {
+        try {
+            int index = FindEl.indexOf(new String[] {"first", "second"}, "third");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
