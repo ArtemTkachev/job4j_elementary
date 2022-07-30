fc60628b5c0bb655aa91d135afcf78b39f646434
ArtemTkachev <art59926@gmail.com>
2022-07-30 21:11:04 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Tkachev Artem");
+        System.out.println("19.08.1987");
+    }
+}
