1dbf1c25132bfb9c90baed9375f161fe116a859a
ArtemTkachev <art59926@gmail.com>
2022-08-04 14:13:59 +0300

    6.4.2. Обход массива с последнего элемента [#361619]

--- src/main/java/ru/job4j/array/RollBackArray2.java
+++ src/main/java/ru/job4j/array/RollBackArray2.java
@@ -1,6 +1,6 @@
 package ru.job4j.array;
 
-public class RollBackArray {
+public class RollBackArray2 {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
         for (int index = 0; index < array.length; index++) {
--- src/test/java/ru/job4j/array/RollBackArray2Test.java
+++ src/test/java/ru/job4j/array/RollBackArray2Test.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RollBackArrayTest {
+public class RollBackArray2Test {
     @Test
     public void whenEmpty() {
         int[] input = new int[] {};
         int[] expected = new int[] {};
-        int[] result = RollBackArray.rollback(input);
+        int[] result = RollBackArray2.rollback(input);
         assertThat(result).containsExactly(expected);
     }
 
@@ -16,7 +16,7 @@ public class RollBackArrayTest {
     public void whenOne() {
         int[] input = new int[] {1};
         int[] expected = new int[] {1};
-        int[] result = RollBackArray.rollback(input);
+        int[] result = RollBackArray2.rollback(input);
         assertThat(result).containsExactly(expected);
     }
 
@@ -24,7 +24,7 @@ public class RollBackArrayTest {
     public void whenFull() {
         int[] input = new int[] {1, 2, 3, 4};
         int[] expected = new int[] {4, 3, 2, 1};
-        int[] result = RollBackArray.rollback(input);
+        int[] result = RollBackArray2.rollback(input);
         assertThat(result).containsExactly(expected);
     }
 
@@ -32,7 +32,7 @@ public class RollBackArrayTest {
     public void whenTheSame() {
         int[] input = new int[] {1, 1, 1, 1};
         int[] expected = new int[] {1, 1, 1, 1};
-        int[] result = RollBackArray.rollback(input);
+        int[] result = RollBackArray2.rollback(input);
         assertThat(result).containsExactly(expected);
     }
 }
